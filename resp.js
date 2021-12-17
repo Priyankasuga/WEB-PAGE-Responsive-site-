1a4465b66c92f91b819db86153aa4d90b78284ff
Priyankasuga <95472741+Priyankasuga@users.noreply.github.com>
2021-12-17 14:45:26 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,11 @@
+burger=document.quaryselector(.burger)
+navbar=document.quaryselector(.navbar)
+navlist=document.quaryselector(.navlist)
+rightnav=document.quaryselector(.rightnav)
+
+burger.addevetnlistner('click'.charAt()=>{
+    navlist.class.toogle('v-class-resp');
+    rightnav.class.toogle('v-class-resp');
+    navbar.class.toogle(' h-nav-resp');
+
+})
